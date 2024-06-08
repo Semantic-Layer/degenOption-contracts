dc56f51b252864128cd32749256f5fdd1df1c654
zoey-t <101599447+zoey-t@users.noreply.github.com>
2024-06-07 17:42:05 -0700

    addliquidity

--- Makefile
+++ Makefile
@@ -7,3 +7,7 @@ deploy:
 .PHONY: mineAddress
 mineAddress:
 	forge script script/Deploy.s.sol:MineAddress --broadcast --rpc-url ${SEPOLIA_RPC_URL} -vvv
+
+.PHONY: addLiquidity
+addLiquidity:
+	forge script script/Deploy.s.sol:AddLiquidity --broadcast --rpc-url ${SEPOLIA_RPC_URL} -vvv
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {PoolManager} from "v4-core/PoolManager.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {PoolModifyLiquidityTest} from "v4-core/test/PoolModifyLiquidityTest.sol";
 import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
@@ -48,7 +49,7 @@ contract Deploy is Script, Deployers {
 contract MineAddress is Script, Deployers {
     uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
     address internal deployer = vm.addr(deployerPrivateKey);
-    address creat2deployerProxy  = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+    address creat2deployerProxy = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
     Create2Deployer create2deployer = Create2Deployer(0x98B2920D53612483F91F12Ed7754E51b4A77919e);
     // Create3Deployer create3deployer = Create3Deployer(0x6513Aedb4D1593BA12e50644401D976aebDc90d8);
     PoolKey poolKey;
@@ -62,28 +63,43 @@ contract MineAddress is Script, Deployers {
     DeployFactory deployFactory = DeployFactory(0x617346FACCe2491B840877e99A638902af54DE0B);
     VolumeTrackerHook hook;
 
+    PoolModifyLiquidityTest poolModifyLiquidityRouter =
+        PoolModifyLiquidityTest(0x2b925D1036E2E17F79CF9bB44ef91B95a3f9a084);
+
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
 
-        (address hookAddress, bytes32 salt) = findAddress(address(creat2deployerProxy));
-        bytes memory deployBytecode = type(VolumeTrackerHook).creationCode;
+        // (address hookAddress, bytes32 salt) = findAddress(address(creat2deployerProxy));
+        // bytes memory deployBytecode = type(VolumeTrackerHook).creationCode;
 
-        console2.log("deployHook");
-        require(deployBytecode.length != 0, "byte zero");
-        bytes memory creationCodeWithArgs = abi.encodePacked(
-            type(VolumeTrackerHook).creationCode, abi.encode(address(poolManager), "", 1, address(OK), deployer)
-        );
+        // console2.log("deployHook");
+        // require(deployBytecode.length != 0, "byte zero");
+        // bytes memory creationCodeWithArgs = abi.encodePacked(
+        //     type(VolumeTrackerHook).creationCode, abi.encode(address(poolManager), "", 1, address(OK), deployer)
+        // );
 
-        // address hookAddr = deployFactory.deploy(salt, creationCodeWithArgs);
-        // address hookAddr = create2deployer.deploy(creationCodeWithArgs, salt);
-        hook = new VolumeTrackerHook{salt:salt}(poolManager, "",1 , address(OK), deployer);
+        // // address hookAddr = deployFactory.deploy(salt, creationCodeWithArgs);
+        // // address hookAddr = create2deployer.deploy(creationCodeWithArgs, salt);
+        // hook = new VolumeTrackerHook{salt: salt}(poolManager, "", 1, address(OK), deployer);
 
-        console2.log("hook deployed at", address(hook));
-        require(hookAddress == address(hook), "wrong deploy");
+        // console2.log("hook deployed at", address(hook));
+        // require(hookAddress == address(hook), "wrong deploy");
 
         console2.log("init pool");
-        poolKey = PoolKey(ethCurrency, tokenCurrency, 3000, 60, IHooks(address(hook)));
-        poolManager.initialize(poolKey, SQRT_PRICE_1_1, ZERO_BYTES);
+        poolKey = PoolKey(ethCurrency, tokenCurrency, 3000, 60, IHooks(0xCd3EB6f3F81b62a4E544Ff513727dE1978078040));
+        // poolManager.initialize(poolKey, SQRT_PRICE_1_1, ZERO_BYTES);
+
+        OK.mint(deployer, 1000 ether);
+        OK.approve(address(poolModifyLiquidityRouter), type(uint256).max);
+        OK.approve(address(poolSwapTest), type(uint256).max);
+
+        console2.log("add liquidity");
+        poolModifyLiquidityRouter.modifyLiquidity{value: 0.003 ether}(
+            poolKey,
+            IPoolManager.ModifyLiquidityParams({tickLower: -60, tickUpper: 60, liquidityDelta: 1 ether, salt: 0}),
+            ZERO_BYTES
+        );
+
         vm.stopBroadcast();
 
         // console2.logBytes(bytecode);
@@ -104,7 +120,35 @@ contract MineAddress is Script, Deployers {
 
         return (hookAddress, salt);
     }
+}
 
+contract AddLiquidity is Script, Deployers {
+    uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+    address internal deployer = vm.addr(deployerPrivateKey);
+    address creat2deployerProxy = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
+    MockERC20 OK = MockERC20(0x9fE71A8fb340E1cC13F61691bCaDDB83aE6c00ac);
+    PoolManager poolManager = PoolManager(0x75E7c1Fd26DeFf28C7d1e82564ad5c24ca10dB14);
+    PoolSwapTest poolSwapTest = PoolSwapTest(0xB8b53649b87F0e1eb3923305490a5cB288083f82);
+    PoolModifyLiquidityTest poolModifyLiquidityRouter =
+        PoolModifyLiquidityTest(0x2b925D1036E2E17F79CF9bB44ef91B95a3f9a084);
+    Currency ethCurrency = Currency.wrap(address(0));
+    Currency tokenCurrency = Currency.wrap(address(OK));
+
+    DeployFactory deployFactory = DeployFactory(0x617346FACCe2491B840877e99A638902af54DE0B);
+    VolumeTrackerHook hook = VolumeTrackerHook(0xCd3EB6f3F81b62a4E544Ff513727dE1978078040);
+
+    PoolKey poolKey = PoolKey(ethCurrency, tokenCurrency, 3000, 60, IHooks(address(hook)));
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+        poolModifyLiquidityRouter.modifyLiquidity{value: 0.003 ether}(
+            key,
+            IPoolManager.ModifyLiquidityParams({tickLower: -60, tickUpper: 60, liquidityDelta: 1 ether, salt: 0}),
+            ZERO_BYTES
+        );
+        vm.stopBroadcast();
+    }
 }
 
 contract DeployFactory {
